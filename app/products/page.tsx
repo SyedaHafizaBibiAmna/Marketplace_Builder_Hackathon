74609ea8ee3587f9077d57f22e0b0b695b5a98cd
SyedaHafizaBibiAmna <159658253+SyedaHafizaBibiAmna@users.noreply.github.com>
2025-02-03 06:43:21 +0500

    new

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,12 +1,45 @@
 
-import ProductList from '../components/productlist'
+// import ProductList from '../components/productlist'
+
+// export const dynamic = "force-dynamic";
+// export default function AllProduct ()  {
+//   return (
+//     <div>
+    
+//     <ProductList/>
+//      </div>
+//   )
+// }
+import { client } from "@/sanity/lib/client";
+import SearchandFilter from "../components/SearchandFilter";
+import ProductList from "../components/productlist";
 
 export const dynamic = "force-dynamic";
-export default function AllProduct ()  {
+
+async function getData() {
+  const query = `*[_type == "product"] | order(_createdAt desc) {
+    _id,
+    title,
+    price,
+    "categoryName": category->name,
+    "imageUrl": image.asset->url,
+    description,
+    badge,
+    priceWithoutDiscount,
+    "tags": tags[]
+  }`;
+
+  const data = await client.fetch(query);
+  return data;
+}
+
+export default async function AllProduct() {
+  const data = await getData();
+
   return (
     <div>
-    
-    <ProductList/>
-     </div>
-  )
+      {/* Pass data to SearchandFilter */}
+      <ProductList  />
+    </div>
+  );
 }
